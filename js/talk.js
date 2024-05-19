dd2454b85b53cf63f13cee9c0bf5d38aea8665a2
ALDEtalle <119827671+JoseAlderete11@users.noreply.github.com>
2024-05-19 12:36:41 -0500

    Add files via upload

---
+++ js/talk.js
@@ -0,0 +1,20 @@
+new TypeIt("#tqyxhj", {
+        loop: true,
+        cursorSpeed: 1000,
+        speed: 100
+    })
+    .type("EMERSON & PAMELA")
+    .pause(2000)
+    .delete(null, {
+        delay: 500
+    })
+    .type("Eres mi  saiyajin favorito    😍")
+    .pause(2000)
+    .go();
+
+new TypeIt('#talkToXHJ', {
+    lifeLike: true,
+    cursorSpeed: 1000,
+    waitUntilVisible: true,
+    speed: 100
+}).go();
